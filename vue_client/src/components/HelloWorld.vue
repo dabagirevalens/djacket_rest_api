b1d4d50ad9011f1cfb2178a4e5730d448d0e8d37
dabagirevalens <vdabagire@gmail.com>
2022-02-19 21:07:21 +0200

    vue client with tailwind css

---
+++ vue_client/src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <span class="font-bold underline text-xl">Valens</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
