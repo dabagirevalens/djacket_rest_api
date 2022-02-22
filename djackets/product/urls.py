a6a69c1c84abec641176f33e4970bc062ad48516
dabagirevalens <vdabagire@gmail.com>
2022-02-22 08:03:16 +0200

    product search

--- djackets/product/urls.py
+++ djackets/product/urls.py
@@ -4,7 +4,7 @@ from product import views
 
 urlpatterns =[
     path('latest-products/', views.LatestProductsList.as_view()),
-    path('products/search/', views.search),
+    path('products/', views.ProductSearch.as_view()),
     path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDeatails.as_view()),
     path('products/<slug:category_slug>/', views.CategoryDetails.as_view())
 ]
